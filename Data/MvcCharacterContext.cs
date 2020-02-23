a3267a2371e9a6f5dabb5f375ca619b795ca22c3
LeoKnox <LeoKnox@users.noreply.github.com>
2020-02-22 21:37:03 -0700

    Add files via upload

---
+++ Data/MvcCharacterContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcCharacter.Models;
+using Microsoft.EntityFrameworkCore;
+using MvcCharacter.Models;
+
+namespace MvcCharacter.Data
+{
+    public class MvcCharacterContext : DbContext
+    {
+        public MvcCharacterContext (DbContextOptions<MvcCharacterContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Character> Character { get; set; }
+        public DbSet<Level> Levels { get; set; }
+        public DbSet<Ability> Abilities { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Ability>().ToTable("Ability");
+            modelBuilder.Entity<Level>().ToTable("Level");
+            modelBuilder.Entity<Character>().ToTable("Character");
+        }
+    }
+}
---
+++ Pages/Characters/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MvcCharacter.Data;
+using MvcCharacter.Models;
+
+namespace MvcCharacter
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly MvcCharacter.Data.MvcCharacterContext _context;
+
+        public DetailsModel(MvcCharacter.Data.MvcCharacterContext context)
+        {
+            _context = context;
+        }
+
+        public Character Character { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Character = await _context.Character.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Character == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
